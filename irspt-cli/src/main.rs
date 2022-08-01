d7c2e803fb5e1a5955b6b7a6c56445854ed0c7f9
João Neves <joao95neves@gmail.com>
2022-08-01 21:21:51 +0100

    [cli/main]: Added error handling for invalid saved struct templates.

--- irspt-cli/src/main.rs
+++ irspt-cli/src/main.rs
@@ -1,9 +1,12 @@
 mod prompt;
 mod validators;
-use irspt_core::{models::InvoiceTemplate, traits::InvoiceTemplateStore};
+use irspt_core::{
+    models::{InvoiceTemplate, IssueInvoiceRequest},
+    traits::InvoiceTemplateStore,
+};
 use irspt_infra::{InvoiceTemplateSledStore, SledDb};
 
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use inquire::Confirm;
 use prompt::prompt_invoice_request;
 
@@ -12,15 +15,15 @@ const DEFAULT_TEMPLATE_NAME: &str = "DEFAULT";
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut sled_db = SledDb::new();
-    let invoice_template_store = Some(InvoiceTemplateSledStore::new(&mut sled_db)?);
+    let invoice_template_store = InvoiceTemplateSledStore::new(&mut sled_db)?;
 
-    let existing_model = match invoice_template_store
-        .as_ref()
-        .unwrap()
-        .get_template(DEFAULT_TEMPLATE_NAME)?
-    {
-        None => None,
-        Some(template) => Some(template.invoice_model),
+    let existing_model = match invoice_template_store.get_template(DEFAULT_TEMPLATE_NAME) {
+        Err(_) => delete_template_if_invalid_prompt(&invoice_template_store)?,
+
+        anyhow::Result::Ok(result) => match result {
+            Some(template) => Some(template.invoice_model),
+            None => None,
+        },
     };
 
     let invoice_request = prompt_invoice_request(&existing_model)?;
@@ -39,15 +42,9 @@ async fn main() -> Result<()> {
         };
 
         if existing_model.is_some() {
-            invoice_template_store
-                .as_ref()
-                .unwrap()
-                .update_template(&template)?;
+            invoice_template_store.update_template(&template)?;
         } else {
-            invoice_template_store
-                .as_ref()
-                .unwrap()
-                .add_template(&template)?;
+            invoice_template_store.add_template(&template)?;
         }
     }
 
@@ -68,4 +65,22 @@ async fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn delete_template_if_invalid_prompt<'a>(
+    invoice_template_store: &impl InvoiceTemplateStore<'a>,
+) -> Result<Option<IssueInvoiceRequest>> {
+    let delete_template = Confirm::new(
+        "ERROR: Your template data is invalid. Delete the existing one to create a new one?",
+    )
+    .with_default(true)
+    .prompt()?;
+
+    if !delete_template {
+        return Ok(None);
+    }
+
+    invoice_template_store.remove_template(&DEFAULT_TEMPLATE_NAME)?;
+
+    Ok(None)
 }
