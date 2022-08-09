81f3ee81778168d43c1f96ab088bf49b2ee3090f
João Neves <joao95neves@gmail.com>
2022-08-09 22:25:58 +0100

    [InvoiceTemplateStore]: Removed `get_saved_template_version` and `set_saved_template_version`

--- irspt-contracts/src/traits/invoice_template_store.rs
+++ irspt-contracts/src/traits/invoice_template_store.rs
@@ -3,10 +3,6 @@ use crate::models::InvoiceTemplate;
 use anyhow::Result;
 
 pub trait InvoiceTemplateStore<'a> {
-    fn get_saved_template_version(&self) -> Result<Option<u8>>;
-
-    fn set_saved_template_version(&self, version: u8) -> Result<()>;
-
     fn get_template(&self, template_name: &str) -> Result<Option<InvoiceTemplate>>;
 
     fn add_template(&self, model: &InvoiceTemplate) -> Result<()>;
--- irspt-infra/src/invoice_template_sled_store.rs
+++ irspt-infra/src/invoice_template_sled_store.rs
@@ -7,7 +7,6 @@ use irspt_contracts::{models::InvoiceTemplate, traits::InvoiceTemplateStore};
 use anyhow::{Ok, Result};
 
 const INVOICE_TEAMPLATES_TABLE_NAME: &str = "INVOICE_TEAMPLATES";
-const INVOICE_TEAMPLATES_VERSION_TABLE_NAME: &str = "INVOICE_TEAMPLATES_V";
 
 pub struct InvoiceTemplateSledStore<'a> {
     sled_db: &'a SledDb,
@@ -24,34 +23,6 @@ impl<'a> InvoiceTemplateSledStore<'a> {
 }
 
 impl<'a> InvoiceTemplateStore<'a> for InvoiceTemplateSledStore<'a> {
-    fn get_saved_template_version(&self) -> Result<Option<u8>> {
-        let raw_result = self
-            .sled_db
-            .db_ref()
-            .unwrap()
-            .open_tree(INVOICE_TEAMPLATES_TABLE_NAME)?
-            .get(INVOICE_TEAMPLATES_VERSION_TABLE_NAME)?;
-
-        if raw_result.is_none() {
-            return Ok(None);
-        }
-
-        Ok(Some(deserialize_from_bytes::<u8>(&raw_result.unwrap())?))
-    }
-
-    fn set_saved_template_version(&self, version: u8) -> Result<()> {
-        self.sled_db
-            .db_ref()
-            .unwrap()
-            .open_tree(INVOICE_TEAMPLATES_TABLE_NAME)?
-            .insert(
-                INVOICE_TEAMPLATES_VERSION_TABLE_NAME,
-                serialize_to_bytes(&version)?,
-            )?;
-
-        Ok(())
-    }
-
     fn get_template(&self, template_name: &str) -> Result<Option<InvoiceTemplate>> {
         let raw_result = self
             .sled_db
