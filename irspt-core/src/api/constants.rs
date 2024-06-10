92a626b952b90aa971cb0731185f8a69781326a4
João Neves <joao95neves@gmail.com>
2024-06-10 16:16:08 +0100

    chore: use `&'static str` for constants

--- irspt-core/src/api/constants.rs
+++ irspt-core/src/api/constants.rs
@@ -1,55 +1,55 @@
 pub struct IrsPtUrls {}
 
 impl IrsPtUrls {
-    pub const LOGIN_PAGE: &str =
+    pub const LOGIN_PAGE: &'static str =
         "https://www.acesso.gov.pt/v2/loginForm?partID=PFAP&path=/geral/home";
 
-    pub const ISSUE_INVOICE_PAGE: &str =
+    pub const ISSUE_INVOICE_PAGE: &'static str =
         "https://irs.portaldasfinancas.gov.pt/recibos/portal/emitir/emitirfatura";
 }
 
 pub struct HtmlTagNames {}
 
 impl HtmlTagNames {
-    pub const SELECT: &str = "select";
+    pub const SELECT: &'static str = "select";
 
-    pub const INPUT: &str = "select";
+    pub const INPUT: &'static str = "select";
 
-    pub const TEXTAREA: &str = "textarea";
+    pub const TEXTAREA: &'static str = "textarea";
 
-    pub const OPTION: &str = "option";
+    pub const OPTION: &'static str = "option";
 }
 
 pub struct HtmlPropertyNames {}
 
 impl HtmlPropertyNames {
-    pub const NAME: &str = "name";
+    pub const NAME: &'static str = "name";
 
-    pub const VALUE: &str = "value";
+    pub const VALUE: &'static str = "value";
 }
 
 pub struct InvoicePageNameValues {}
 
 impl InvoicePageNameValues {
-    pub const SERVICE_DATE: &str = "dataPrestacao";
+    pub const SERVICE_DATE: &'static str = "dataPrestacao";
 
-    pub const INVOICE_TYPE: &str = "tipoRecibo";
+    pub const INVOICE_TYPE: &'static str = "tipoRecibo";
 
-    pub const CLIENT_COUNTRY: &str = "pais";
+    pub const CLIENT_COUNTRY: &'static str = "pais";
 
-    pub const VAT_NATIONAL_CLIENT: &str = "nifAdquirente";
+    pub const VAT_NATIONAL_CLIENT: &'static str = "nifAdquirente";
 
-    pub const VAT_INTERNATIONAL_CLIENT: &str = "nifEstrangeiro";
+    pub const VAT_INTERNATIONAL_CLIENT: &'static str = "nifEstrangeiro";
 
-    pub const CLIENT_NAME: &str = "nomeAdquirente";
+    pub const CLIENT_NAME: &'static str = "nomeAdquirente";
 
-    pub const CLIENT_ADDRESS: &str = "moradaAdquirente";
+    pub const CLIENT_ADDRESS: &'static str = "moradaAdquirente";
 
-    pub const SERVICE_DESCRIPTION: &str = "servicoPrestado";
+    pub const SERVICE_DESCRIPTION: &'static str = "servicoPrestado";
 
-    pub const SERVICE_VALUE: &str = "valorBase";
+    pub const SERVICE_VALUE: &'static str = "valorBase";
 
-    pub const REGIME_IVA: &str = "regimeIva";
+    pub const REGIME_IVA: &'static str = "regimeIva";
 
-    pub const REGIME_INCIDENCIA_IRS: &str = "regimeIncidenciaIrs";
+    pub const REGIME_INCIDENCIA_IRS: &'static str = "regimeIncidenciaIrs";
 }
